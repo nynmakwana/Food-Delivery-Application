9e885766abdcd5bbf575d95d65045f0d4db0fa02
Priyanshu Desai <pdesai1@seattleu.edu>
2025-03-13 02:40:43 -0700

    Created Frontend for  our Food Delivery Website

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './Footer.css'
+import { assets } from '../../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='footer' id='footer'>
+      <div className="footer-content">
+        <div className="footer-content-left">
+            <img src={assets.logo} alt="" />
+            <p>MNPS Food Delivery App</p>
+            <div className="footer-social-icons">
+                
+            </div>
+        </div>
+        <div className="footer-content-center">
+            <h2>COMPANY</h2>
+            <ul>
+                <li>Home</li>
+                <li>About us</li>
+                <li>Delivery</li>
+                <li>Privacy policy</li>
+            </ul>
+        </div>
+        
+      </div>
+      <hr />
+      <p className="footer-copyright">Copyright 2025 © MNPS- All Right Reserved.</p>
+    </div>
+  )
+}
+
+export default Footer
---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className='header-contents'>
+                <h2>What would you like to have today? </h2>
+                <p>Explore our handcrafted menu featuring fresh, locally-sourced ingredients prepared by expert chefs. From comfort classics to innovative creations, we deliver exceptional flavors straight to your door..</p>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Header
